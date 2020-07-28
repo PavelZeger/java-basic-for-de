c9b05d8a8c4c48249714ec8914775d5dd9651410
Pavel Zeger <gabberoid@gmail.com>
2020-07-28 21:05:24 +0300

    Update MainLab07.java

--- src/main/java/lab07/MainLab07.java
+++ src/main/java/lab07/MainLab07.java
@@ -13,20 +13,20 @@ public class MainLab07 {
 
 
         DateTimeFormatter formatterDate = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-        DateFormatUtil dateFormatUtilDate = new DateFormatUtil(formatterDate);
+        DateFormatService dateFormatServiceDate = new DateFormatService(formatterDate);
 
         DateTimeFormatter formatterDateTime = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-        DateFormatUtil dateFormatUtilDateTime = new DateFormatUtil(formatterDateTime);
+        DateFormatService dateFormatServiceDateTime = new DateFormatService(formatterDateTime);
 
         LocalDate date = LocalDate.of(2017, 7, 25);
         LocalDateTime dateTime = LocalDateTime.of(2017, 8, 10, 13, 45);
         String dateString = formatterDate.format(date);
         String dateTimeString = formatterDateTime.format(dateTime);
 
-        System.out.println(dateFormatUtilDate.convert(date));
-        System.out.println(dateFormatUtilDateTime.convert(dateTime));
+        System.out.println(dateFormatServiceDate.convert(date));
+        System.out.println(dateFormatServiceDateTime.convert(dateTime));
 
-        System.out.println(dateFormatUtilDate.convert(dateString));
-        System.out.println(dateFormatUtilDateTime.convertToLocalDateTime(dateTimeString));
+        System.out.println(dateFormatServiceDate.convert(dateString));
+        System.out.println(dateFormatServiceDateTime.convertToLocalDateTime(dateTimeString));
     }
 }
