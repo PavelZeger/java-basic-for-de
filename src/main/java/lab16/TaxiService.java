04aea1999dc371b89614098d274d6652ca24b9bf
PavelZeger <gabberoid@gmail.com>
2020-10-11 17:10:59 +0300

    Updated lab16

--- src/main/java/lab16/TaxiService.java
+++ src/main/java/lab16/TaxiService.java
@@ -1,6 +1,7 @@
 package lab16;
 
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import scala.Tuple2;
@@ -12,6 +13,7 @@ public class TaxiService {
     public static void main(String[] args) {
         SparkConf sparkConfiguration = new SparkConfiguration().developSparkConfiguration();
         JavaSparkContext sparkContext = new SparkConfiguration().javaSparkContext(sparkConfiguration);
+
         JavaRDD<String> orders = sparkContext.textFile("src\\main\\resources\\taxi_orders.txt", 1);
         JavaRDD<String> drivers = sparkContext.textFile("src\\main\\resources\\drivers.txt", 1);
 
@@ -27,18 +29,27 @@ public class TaxiService {
                 .map(row -> Integer.parseInt(row.split("\\s")[2]))
                 .reduce(Integer::sum);
 
+
+        JavaPairRDD<Integer, Integer> driversRdd = orders
+                .map(row -> row.split("\\s"))
+                .mapToPair(elem -> new Tuple2<>(Integer.parseInt(elem[0]),Integer.parseInt(elem[2])))
+                .reduceByKey(Integer::sum)
+                .mapToPair(Tuple2::swap)
+                .mapToPair(Tuple2::swap);
+
+        JavaPairRDD<Integer, String> namesRdd = drivers
+                .map(row -> row.split(", "))
+                .mapToPair(elem -> new Tuple2<>(Integer.parseInt(elem[0]), elem[1]));
+
+        JavaPairRDD<Integer, Tuple2<Integer, String>> joinRdd = driversRdd
+                .join(namesRdd)
+                .sortByKey(false);
+
         System.out.format("Number of lines in `taxi_orders.txt`: %d\n", numberOfRows);
         System.out.format("Number of trips to Boston longer than 10 km: %d\n", numberOfTrips);
-        System.out.printf("Amount of all trips to Boston in kilometers: %d\n", sumOfKms);
-
-        //Write names of 3 drivers with max total kilometers in this day(sort top to down)
-        orders.mapToPair(row -> new Tuple2<>(row.split("\\s")[0], Integer.parseInt(row.split("\\s")[2])))
-                .collect()
-                .forEach(System.out::println);
-
-
+        System.out.format("Amount of all trips to Boston in kilometers: %d\n", sumOfKms);
+        joinRdd.take(3).forEach(row -> System.out.format("Driver with biggest amount of kms: %s\n", row._2._2()));
 
     }
-
 
 }
